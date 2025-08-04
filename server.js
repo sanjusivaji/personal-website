ac2d031ed8983a4b37f320e9bc7aec91f62af26a
sanjusivaji <sajithpecoder@gmail.com>
2025-08-04 14:55:34 +0530

    Create server.js
    Creates server.js file

---
+++ server.js
@@ -0,0 +1,50 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const nodemailer = require("nodemailer");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+
+app.get("/", (req, res) => {
+    res.send("Server is running");
+  });
+
+app.post("/contact", (req, res) => {
+  const { name, email, subject, message } = req.body;
+  console.log("Received req", req.body);
+
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.USERNAME,
+        pass: process.env.PASSWORD
+    }
+  });
+
+  const mailOptions = {
+    from: email,
+    to: "sanjusivaji@gmail.com",
+    subject: subject,
+    text: `Name: ${name}\nEmail: ${email}\n\n${message}`
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log("Mail error:", error);
+      return res.status(500).send("error");
+    }
+    console.log("Mail sent:", info.response);
+    res.send("success");
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server started on port 3000");
+});
