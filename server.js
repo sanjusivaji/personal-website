3ab7b0c75484f97a19d3618f6cd5f12fe001e97a
sanjusivaji <sajithpecoder@gmail.com>
2025-08-05 23:05:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const nodemailer = require("nodemailer");
+require("dotenv").config();
+
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
