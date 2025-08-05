d48cd90f09ed293720201f20168932c0b503537f
sanjusivaji <sajithpecoder@gmail.com>
2025-08-05 18:27:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -47,6 +47,11 @@ app.post("/contact", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 
+// app.listen(3000, () => {
+//   console.log("Server started on port 3000");
+// });
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
