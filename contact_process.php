7d386cdd0f795cdcb4320180f46e917db9a5e60e
sanjusivaji <sajithpecoder@gmail.com>
2025-08-06 07:41:47 +0530

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -1,76 +1,24 @@
-// <?php
-
-// $to = "spn8@spondonit.com";
-
-// $from = filter_var($_REQUEST['email'], FILTER_SANITIZE_EMAIL);
-// $name = htmlspecialchars($_REQUEST['name']);
-// $csubject = htmlspecialchars($_REQUEST['subject']);
-// $number = htmlspecialchars($_REQUEST['number']);
-// $cmessage = htmlspecialchars($_REQUEST['message']);
-
-// $headers = "From: " . $from . "\r\n";
-// $headers .= "Reply-To: " . $from . "\r\n";
-// $headers .= "MIME-Version: 1.0\r\n";
-// $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-// $subject = "You have a message from your Bitmap Photography.";
-
-// $logo = 'img/logo.png';
-// $link = '#';
-
-// $body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-// $body .= "<table style='width: 100%;'>";
-// $body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-// $body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
-// $body .= "</td></tr></thead><tbody>";
-// $body .= "<tr><td style='border:none;'><strong>Name:</strong> {$name}</td>";
-// $body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td></tr>";
-// $body .= "<tr><td style='border:none;'><strong>Phone:</strong> {$number}</td></tr>";
-// $body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
-// $body .= "<tr><td colspan='2' style='border:none;'><p>{$cmessage}</p></td></tr>";
-// $body .= "</tbody></table>";
-// $body .= "</body></html>";
-
-// if (filter_var($from, FILTER_VALIDATE_EMAIL) && !empty($name) && !empty($cmessage)) {
-//     $send = mail($to, $subject, $body, $headers);
-// }
-
-// ?>
-
 <?php
+$to = "sanjusivaji@gmail.com";
+$from = $_POST['email'];
+$name = $_POST['name'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
 
-$to = "sanjusivaji@gmail.com";  // ← change to your real email
+$headers = "From: $from\r\n";
+$headers .= "Reply-To: $from\r\n";
+$headers .= "Content-Type: text/html; charset=UTF-8\r\n";
 
-$from = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-$name = htmlspecialchars($_POST['name']);
-$csubject = htmlspecialchars($_POST['subject']);
-$cmessage = htmlspecialchars($_POST['message']);
+$body = "<strong>Name:</strong> $name<br>";
+$body .= "<strong>Email:</strong> $from<br>";
+$body .= "<strong>Subject:</strong> $subject<br>";
+$body .= "<strong>Message:</strong><br>$message";
 
-$headers = "From: " . $from . "\r\n";
-$headers .= "Reply-To: " . $from . "\r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-$subject = "New message from your website";
-
-$body = "<html><body>";
-$body .= "<h2>New Contact Message</h2>";
-$body .= "<p><strong>Name:</strong> {$name}</p>";
-$body .= "<p><strong>Email:</strong> {$from}</p>";
-$body .= "<p><strong>Subject:</strong> {$csubject}</p>";
-$body .= "<p><strong>Message:</strong><br>{$cmessage}</p>";
-$body .= "</body></html>";
-
-if (filter_var($from, FILTER_VALIDATE_EMAIL)) {
-    $send = mail($to, $subject, $body, $headers);
-    if ($send) {
-        echo "Message sent successfully.";
-    } else {
-        echo "Failed to send message.";
-    }
+if (mail($to, "Message from Contact Form", $body, $headers)) {
+    echo "Message sent successfully!";
 } else {
-    echo "Invalid email address.";
+    echo "Failed to send message.";
 }
-
 ?>
+
 
