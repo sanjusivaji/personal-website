55c7dc6e994d3424b82f1fb30710e62f6583099a
sanjusivaji <sajithpecoder@gmail.com>
2025-08-06 07:25:57 +0530

    Update contact_process.php

--- contact_process.php
+++ contact_process.php
@@ -1,37 +1,38 @@
 <?php
 
-    $to = "spn8@spondonit.com";
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
+$to = "spn8@spondonit.com";
 
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+$from = filter_var($_REQUEST['email'], FILTER_SANITIZE_EMAIL);
+$name = htmlspecialchars($_REQUEST['name']);
+$csubject = htmlspecialchars($_REQUEST['subject']);
+$number = htmlspecialchars($_REQUEST['number']);
+$cmessage = htmlspecialchars($_REQUEST['message']);
 
-    $subject = "You have a message from your Bitmap Photography.";
+$headers = "From: " . $from . "\r\n";
+$headers .= "Reply-To: " . $from . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $logo = 'img/logo.png';
-    $link = '#';
+$subject = "You have a message from your Bitmap Photography.";
 
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
-	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
-	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
+$logo = 'img/logo.png';
+$link = '#';
 
+$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
+$body .= "<table style='width: 100%;'>";
+$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
+$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
+$body .= "</td></tr></thead><tbody>";
+$body .= "<tr><td style='border:none;'><strong>Name:</strong> {$name}</td>";
+$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td></tr>";
+$body .= "<tr><td style='border:none;'><strong>Phone:</strong> {$number}</td></tr>";
+$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
+$body .= "<tr><td colspan='2' style='border:none;'><p>{$cmessage}</p></td></tr>";
+$body .= "</tbody></table>";
+$body .= "</body></html>";
+
+if (filter_var($from, FILTER_VALIDATE_EMAIL) && !empty($name) && !empty($cmessage)) {
     $send = mail($to, $subject, $body, $headers);
+}
 
 ?>
