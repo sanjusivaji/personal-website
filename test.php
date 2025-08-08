1579982790625f17a7374030086307de19c1b311
sanjusivaji <sajithpecoder@gmail.com>
2025-08-08 22:38:59 +0530

    Delete test.php

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    echo "POST request received. Server supports PHP.";
-} else {
-    echo "This is a GET request. Try submitting a POST form.";
-}
-?>
